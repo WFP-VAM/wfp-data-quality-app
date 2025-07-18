2863ab49b4dc2fc10e7e6400854824b3aa48e1de
AlexGherardelli <alessandra.gherardelli@wfp.org>
2025-07-18 10:24:13 +0200

    fix: HTML issue on home tab

--- inst/app/app.R
+++ inst/app/app.R
@@ -4,32 +4,7 @@
 #The app was developed by WFP WACARO RAM Team with support from all COs.
 #The app is structure in 3 main points: User Interface (UI), Server Code and Report Template.
 ############################
-library(devtools)
-library(dplyr)
-library(DT)
-library(forcats)
-library(ggplot2)
-library(glue)
-library(haven)
-library(htmltools)
-library(httr)
-library(jsonlite)
-library(kableExtra)
-library(labelled)
-library(lubridate)
-library(openxlsx)
-library(plotly)
-library(purrr)
-library(rlang)
-library(rmarkdown)
-library(rstatix)
-library(scales)
-library(shiny)
-library(shinydashboard)
-library(tidyr)
-library(tidyverse)
-library(treemapify) # for treemap
-library(writexl)
+
 
 source("ui.R")
 source("server.R")
